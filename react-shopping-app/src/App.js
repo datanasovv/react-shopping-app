a0ecb42134d45260c44a07984e29955285ac94ce
Dimitar Atanasov <75276016+datanasovv@users.noreply.github.com>
2022-04-11 16:33:32 +0300

    create product component

---
+++ react-shopping-app/src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Products from "./components/Products";
+import data from "./data.json";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping App</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products} />
+            </div>
+            <div className="sidebar">Card Items</div>
+          </div>
+        </main>
+        <footer>All Rights Reserved.</footer>
+      </div>
+    );
+  }
+}
+
+export default App;
